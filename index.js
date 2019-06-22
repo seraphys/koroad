80cd143c6bc4a0dbb443f127bda528b97d467eed
LEE JAKE <33084462+seraphys@users.noreply.github.com>
2019-06-22 19:48:38 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html><html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width" />
+  <title>Hello, world!</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<h1>도로교통공단 기술지원 업무매뉴얼</h1>
+	<div id="intro"></div>
+	<img src="img/icon.png">
+	<select>
+</body>
+</html>
