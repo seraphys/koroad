34fcea1024fb7883917e672fb33fb372adcbef69
LEE JAKE <33084462+seraphys@users.noreply.github.com>
2019-06-22 23:49:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
 </head>
 <body>
 	<h1>도로교통공단 기술지원 업무매뉴얼</h1>
-	<div id="intro"></div>
+	<div id="intro">
+	<h3>본 페이지는 도로교통공단 안전업무 향상을 위한 페이지입니다.</h3>
+		</div>
 	<img src="img/icon.png">
 	<select>
 		<option></option>
